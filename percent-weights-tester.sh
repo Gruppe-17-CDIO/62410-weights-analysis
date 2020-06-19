ba9c46780125ec24a5a1febf1c2d051c712a759c
s185120 <s185120@student.dtu.dk>
2020-06-19 03:23:28 +0200

    Tilføjet tidligere sh filer

---
+++ percent-weights-tester.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+list=("v3-tiny-14200" "v3-tiny-17600" "v3-tiny-18500" "v3-tiny-20000" "v3-tiny-21000" "v3-tiny-22000" "v3-retrained")
+
+cd darknet
+mkdir test-results
+
+# Calculation
+for index in ${list[*]}; do
+
+    echo -e "Testing on ${index}"
+
+    avg=0
+
+    ./darknet detector test cfg/owndata.data test-files/${index}.cfg test-files/${index}.weights \
+    test-images/all-clubs.png | cut -d " " -f 2 | cut -d "%" -f 1 >> test-results/${index}-percents.txt \
+    && sed -i '1d' test-results/${index}-percents.txt
+
+    while read line; do
+        avg=$(( $avg + $line ))
+    done < test-results/${index}-percents.txt
+
+    avg=$(( $avg / $(wc -l test-results/${index}-percents.txt | cut -d " " -f 1) ))
+    echo "AVG IS ${avg}%" && echo "AVG IS ${avg}%" >> test-results/${index}-percents.txt
+
+done
+
+# Printing the results
+for index in ${list[*]}; do
+    echo "${index}: $(tail -1 test-results/${index}-percents.txt)"
+done
---
+++ weights-tester.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+list=("v3-tiny-14200" "v3-tiny-17600" "v3-tiny-18500" "v3-tiny-20000" "v3-tiny-21000" "v3-tiny-22000" "v3-retrained")
+
+cd darknet
+mkdir test-results
+mkdir test-results/overlaying-clubs-diamonds
+mkdir test-results/overlaying-spades-hearts
+
+for index in ${list[*]}; do
+
+    echo -e "Testing on ${index}"
+
+    ./darknet detector test cfg/owndata.data test-files/${index}.cfg test-files/${index}.weights \
+    test-images/overlaying-clubs-diamonds.png > test-results/overlaying-clubs-diamonds/${index}-output.txt \
+    && mv predictions.jpg test-results/overlaying-clubs-diamonds/${index}-predictions.jpg
+
+    ./darknet detector test cfg/owndata.data test-files/${index}.cfg test-files/${index}.weights \
+    test-images/overlaying-spades-hearts.png > test-results/overlaying-spades-hearts/${index}-output.txt \
+    && mv predictions.jpg test-results/overlaying-spades-hearts/${index}-predictions.jpg
+
+done

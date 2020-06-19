0f229845f4040c21914159f3a831406d7d2f7403
s185120 <s185120@student.dtu.dk>
2020-06-19 16:56:50 +0200

    Forbedret darknet mappe anvendelse og lidt generelt om fil placeringer

--- full-weights-analysis.zsh
+++ full-weights-analysis.zsh
@@ -29,22 +29,44 @@ else
 fi
 
 
-# Make or remake the test-results directory
-if [ -d "test-results" ]; then
-	rm -rf test-results && echo "Removed the test-results directory."
+# Make or remake the test-output directory
+if [ -d "test-output" ]; then
+    while [[ true ]]; do
+        echo "Found an existing test-output directory. Would you like to delete it?"
+        echo "y/n"
+        read answer
+        if [ "${answer}" = "y" ]; then
+            echo "Deleting test-output directory"
+            rm -rf test-output
+            break
+        elif [ "${answer}" = "n" ]; then
+            echo "Cannot proceed without deleting test-output directory."
+            exit
+        fi
+        echo "Unrecognized input."
+    done
 fi
-mkdir test-results && echo "Created the test-results directory."
+mkdir test-output && echo "Created a test-output directory."
 
 
-path_collected_results="test-results/analysis-collected-results.out"
-echo "---------- collected analysis results ----------" >> ${path_collected_results}
+path_collected_results="analysis-collected-results.out"
+echo "---------- collected analysis results ----------" > ${path_collected_results}
 echo "How to read output:" >> ${path_collected_results}
-echo "darknet execution time .....: time in seconds for darknet to run its detection" >> ${path_collected_results}
-echo "average detection percentage: darknets average detection percent for each card" >> ${path_collected_results}
-echo "correct detection ..........: total corners detected with correct suit and value / total corners visible" >> ${path_collected_results}
-echo "false detection ............: total corners detected with a wrong suit / total corners detected" >> ${path_collected_results}
-echo "ignored cards ..............: total cards without any detected corner / total cards visible" >> ${path_collected_results}
+echo "darknet execution time .....: time in seconds for darknet to run its detection" \
+>> ${path_collected_results}
+echo "average detection percentage: darknets average detection percent for each card" \
+>> ${path_collected_results}
+echo "correct detection ..........: total corners detected with correct suit and value / total corners visible" \
+>> ${path_collected_results}
+echo "false detection ............: total corners detected with a wrong suit / total corners detected" \
+>> ${path_collected_results}
+echo "ignored cards ..............: total cards without any detected corner / total cards visible" \
+>> ${path_collected_results}
 echo "" >> ${path_collected_results}
+
+
+path_temp_sorted_actual="test-output/temp_sorted_actual.txt"
+path_temp_sorted_detected="test-output/temp_sorted_detected.txt"
 
 
 # Data analysis loop, goes through each image for each weights file
@@ -65,7 +87,7 @@ for weight_name in ${weights[*]}; do
     # The paths of existing files and result saving destination
     path_file_cfg="test-files/${weight_name}.cfg"
 	path_file_weights="test-files/${weight_name}.weights"
-	path_result_percentages="test-results/${weight_name}-percents.txt"
+	path_result_percentages="test-output/${weight_name}-percents.txt"
 
 
     echo "performin analysis on ${weight_name}"
@@ -76,8 +98,8 @@ for weight_name in ${weights[*]}; do
         # Path of image and results destinations
         path_image_png="test-images/${image_name}.png"
         path_image_txt="test-images/${image_name}.txt"
-        path_result_full="test-results/${weight_name}-${image_name}.out"
-        path_result_cards="test-results/${weight_name}-${image_name}-cards.txt"
+        path_result_full="test-output/${weight_name}-${image_name}.out"
+        path_result_cards="test-output/${weight_name}-${image_name}-cards.txt"
 
 
         # Darknet performing its detection and saving the output
@@ -105,23 +127,23 @@ for weight_name in ${weights[*]}; do
 
         # Find how many correct guesses it had and how many wrong
         guesses=$(( ${guesses} + $(cat ${path_result_cards} | wc -l) ))
-        sort ${path_image_txt} | uniq > temp_sorted_actual.txt
-        sort ${path_result_cards} | uniq > temp_sorted_detected.txt
+        sort ${path_image_txt} | uniq > ${path_temp_sorted_actual}
+        sort ${path_result_cards} | uniq > ${path_temp_sorted_detected}
 
         while read possible; do
             hits=$(( ${hits} + $(cat ${path_result_cards} | grep ${possible} | wc -l) ))
             sed -i "/${possible}/d" ${path_result_cards}
-        done < temp_sorted_actual.txt
+        done < ${path_temp_sorted_actual}
         misses=$(( ${misses} + $(cat ${path_result_cards} | wc -l) ))
 
-        number_of_cards=$(( ${number_of_cards} + $(cat temp_sorted_actual.txt | wc -l) ))
+        number_of_cards=$(( ${number_of_cards} + $(cat ${path_temp_sorted_actual} | wc -l) ))
         while read detection; do
-            sed -i "/${detection}/d" temp_sorted_actual.txt
-        done < temp_sorted_detected.txt
-        ignored=$(( ${ignored} + $(cat temp_sorted_actual.txt | wc -l) ))
+            sed -i "/${detection}/d" ${path_temp_sorted_actual}
+        done < ${path_temp_sorted_detected}
+        ignored=$(( ${ignored} + $(cat ${path_temp_sorted_actual} | wc -l) ))
 
-        rm temp_sorted_actual.txt
-        rm temp_sorted_detected.txt
+        rm ${path_temp_sorted_actual}
+        rm ${path_temp_sorted_detected}
 
 
         # Print the collective hits as of this image
@@ -134,14 +156,13 @@ for weight_name in ${weights[*]}; do
         twodecimals=$(printf '%.2f' \
         "$((${misses}.0/${guesses}.0*100.0))") \
         && echo \
-        "false detection ${misses}/${guesses} (~ ${twodecimals}%)"
+        "falsely detected suits ${misses}/${guesses} (~ ${twodecimals}%)"
 
         # Print the collective ignored as of this image
         twodecimals=$(printf '%.2f' \
         "$((${ignored}.0/${number_of_cards}.0*100.0))") \
         && echo \
         "ignored cards ${ignored}/${number_of_cards} (~ ${twodecimals}%)"
-
     done
 
     echo "${weight_name} results:" >> ${path_collected_results}
@@ -175,7 +196,7 @@ for weight_name in ${weights[*]}; do
     twodecimals=$(printf '%.2f' \
     "$((${misses}.0/${guesses}.0*100.0))") \
     && echo -e \
-    "false detection ............ ${misses}/${guesses} (~ ${twodecimals}%)" \
+    "falsely detected suits ..... ${misses}/${guesses} (~ ${twodecimals}%)" \
     >> ${path_collected_results}
 
     # Save information of ignored cards with all images processed
@@ -189,9 +210,6 @@ for weight_name in ${weights[*]}; do
     echo "-" >> ${path_collected_results}
 done
 
-
 # Printing everything in the collected results txt file
 clear
-while read result; do
-    echo "${result}"
-done < ${path_collected_results}
+cat ${path_collected_results}
